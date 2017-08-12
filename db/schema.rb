2287c022f30c3562bccc314eab0dee2a8826c710
dejanle <dejnik978@gmail.com>
2017-08-12 02:50:25 +0100

    add style to  blog

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170812004510) do
+ActiveRecord::Schema.define(version: 20170812014405) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20170812004510) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image"
+    t.integer "user_id"
   end
 
   create_table "comments", force: :cascade do |t|
